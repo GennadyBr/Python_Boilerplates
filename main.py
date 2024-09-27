64c8db76a1ff5e6889d715b6a2a94d28858c6f6b
GennadyBr <gennady.budazhapov@gmail.com>
2024-09-27 17:14:32 +0300

    fastapi hello world

--- main.py
+++ main.py
@@ -1,47 +1,18 @@
 """ FastAPI Boilerplate Library """
 import logging
-import os
-import sys
-from typing import List
+import uvicorn
+from fastapi import FastAPI
 
 logger = logging.getLogger(__name__)
 
-
-def print_hello(user_name: str = 'DEFAULT', list_a: List | None = None) -> str:
-    """
-    :param user_name: str
-    :param list_a: List
-    :return: str
-    """
-    abc: List = sys.path
-    bcd: str = str(os.path)
-
-    return f'Hello, {user_name}///// {list_a}///// {abc}///// {bcd}'
+app = FastAPI()
 
 
-def append_to_list(value: int, my_list: List | None = None) -> List:
-    """
-    :param value: int
-    :param my_list: List
-    :return: List
-    """
-    if my_list is None:
-        my_list = []
-    new_value = [
-        2,
-        3,
-        value,
-    ]
-    my_list.append(new_value)
-    return my_list
+@app.get('/hello')
+async def hello():
+    """ hello world """
+    return {'message': 'Hello World'}
 
 
 if __name__ == '__main__':
-    name: str = input("What's your name? ")
-    logger.info(print_hello(name))
-
-    result1 = append_to_list(1)
-    logger.info(result1)  # Вывод: [1]
-
-    result2 = append_to_list(2)
-    logger.info(result2)  # Вывод: [1, 2]
+    uvicorn.run(app='main:app', host='0.0.0.0', port=8000, reload=True)
