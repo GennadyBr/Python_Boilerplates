9a5baf78fbb19044109d1861fa8f057191c9fe42
GennadyBr <gennady.budazhapov@gmail.com>
2024-10-04 14:49:53 +0300

    mkdir logs folders

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,11 @@
 import os
 
+def _mk_log_dir(folder: str) -> None:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
-log_filename = f'{os.path.dirname(os.path.abspath(__file__))}/log_conf.yaml'
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(cur_dir)
+log_filename = f'{cur_dir}/log_conf.yaml'
+_mk_log_dir(f'{parent_dir}/logs')
+_mk_log_dir(f'{os.path.dirname(parent_dir)}/tests/logs')
